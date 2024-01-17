b9c351a95bd474e82ca3e3145b09cc900ca346e9
Eesha Shetty <ee.shetty@gmail.com>
2024-01-18 00:23:04 +0530

    error handling

--- app.py
+++ app.py
@@ -11,20 +11,24 @@ def home():
 # API at /api/v1/analysis/ 
 @app.route("/api/v1/analysis/", methods=['GET'])
 def analysis():
+    # Try to get the URI from the JSON
     try:
         get_json = request.get_json()
         image_uri = get_json['uri']
-        
+    except:
+        return jsonify({'error': 'Missing URI in JSON'}), 400
+    
+    # Try to get the text from the image
+    try:
         res = read_image(image_uri)
         
         response_data = {
             "text": res
         }
-
     
         return jsonify(response_data), 200
     except:
-        return jsonify({'error': 'Missing URI in JSON'}), 400
+        return jsonify({'error': 'Error in processing'}), 500
 
 
 if __name__ == "__main__":
